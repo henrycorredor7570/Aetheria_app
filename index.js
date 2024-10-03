63061bb3e2b4d34388c095c0f39fca8f136077e6
Henry Corredor <henry.corredor7570@gmail.com>
2024-10-03 11:41:40 -0500

    project structure

---
+++ index.js
@@ -0,0 +1,10 @@
+import { app }  from "./src/app.js";
+import { conn } from "./src/db.js";
+require('dotenv').config();
+const PORT = process.env.PORT || 3000;
+
+conn.sync({ alter: true }).then(() => {
+    app(PORT, () => {
+        console.log(`server connected to port: ${PORT}`);
+    })
+})
---
+++ src/app.js
@@ -0,0 +1,42 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
+
+const app = express();
+app.name = "API";
+
+app.use(bodyParser.urlencoded({
+    limit: "50mb",
+    extended: true,
+    parameterLimit: 1000000
+}));
+
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(cookieParser());
+app.use(morgan("dev"));
+app.use((req,res,next) => {
+    res.header("Access-Control-Allow-Origin", "*"); //ajustar en produccion: res.header("Access-Control-Allow-Origin", "https://tu-dominio.com");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept, Authorization, x-access-token");
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+    next();
+});
+app.options("", (req, res) => {
+    res.header("Access-Control-Allow-Origin", "*"); //ajustar en produccion: res.header("Access-Control-Allow-Origin", "https://tu-dominio.com");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept, Authorization, x-access-token");
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+    res.sendStatus(204);
+});
+
+app.use("/", routes);
+
+server.use((err, req, res, next) => {
+    const status = err.status || 500;
+    const message = err.message || err;
+    console.error(err);
+    res.status(status).send(message);
+});
+
+export default app;
