227a56eaa5a027f88d80e885f6bc474baf58aa26
Henry Corredor <henry.corredor7570@gmail.com>
2024-11-15 13:12:01 -0500

    updated controllers

--- src/controllers/destinationsController.js
+++ src/controllers/destinationsController.js
@@ -14,6 +14,9 @@ export const getDestinations = async (req, res) => {
 export const createDestination = async (req,res) => {
     const { name, description, latitude, longitude, country, image_url } = req.body;
     try {
+        if(!name || !description || !latitude || !longitude || !country){
+            return res.status(400).json({error:"¡Hizo falta algún dato!"})
+        }
         const destinationCreated = await Destination.create({
             name, 
             description, 
@@ -22,8 +25,47 @@ export const createDestination = async (req,res) => {
             country, 
             image_url
         })
-        res.status(200).json(destinationCreated)
+        res.status(201).json(destinationCreated)
     } catch (error) {
         res.status(500).json({error:error.message})
+    }
+}
+
+export const getDestinationById = async (req,res) => {
+    const { id }= req.params;
+    try {
+        if(isNaN(id)){
+            return res.status(400).json({error:"El ID debe ser un número."});
+        }
+        const destination = await Destination.findByPk(id);
+        if(!destination){
+            return res.status(404).json({error:"Destino no encontrado"});
+        }
+        res.status(200).json(destination);        
+    } catch (error) {
+        res.status(500).json({error:error.message})
+    }
+}
+
+export const updateDestination = async (req,res) => {
+    const { id } = req.params;
+    const { name, description, latitude, longitude, country, image_url } = req.body;
+    try {
+        if(isNaN(id)) return res.status(400).json({error: "El ID debe ser un número"})
+        const destination = await Destination.findByPk(id);
+        if(!destination) return res.status(404).json({error:"Destino no encontrado."})
+        if(!name && !description && !latitude && !longitude && !country && !image_url) return res.status(400).json({error: "Proporcione al menos un campo para actualizar"})
+        
+        destination.name = name || destination.name;
+        destination.description = description || destination.description;
+        destination.latitude = latitude || destination.latitude;
+        destination.longitude = longitude || destination.longitude;
+        destination.country  = country || destination.country;
+        destination.image_url = image_url || destination.image_url;
+        
+        await destination.save();
+        res.status(200).json(destination);
+    } catch (error) {
+        res.status(500).json({error: error.message})
     }
 }
--- src/routes/destinationsRouter.js
+++ src/routes/destinationsRouter.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import  { getDestinations, createDestination }  from "../controllers/destinationsController.js";
+import  { getDestinations, createDestination, getDestinationById, updateDestination }  from "../controllers/destinationsController.js";
 const destinationsRouter = Router();
 
 destinationsRouter.get("", getDestinations);
 destinationsRouter.post("", createDestination);
+destinationsRouter.get("/:id", getDestinationById);
+destinationsRouter.put("/:id", updateDestination);
 
 export default destinationsRouter;
