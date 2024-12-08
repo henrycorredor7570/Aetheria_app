f3ff54ae96a51bc4e8b26351b46cb52b7aa72d8f
Henry Corredor González <78055527+henrycorredor7570@users.noreply.github.com>
2024-12-08 18:27:09 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import app from "./src/app.js";
 import { conn } from "./src/db.js"; 
-import dotenv from 'dotenv';
+import dotenv from 'dotenv'; 
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 
