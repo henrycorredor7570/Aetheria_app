b7a02f263f77d1283cf4d823edc05dac2d8e1741
Henry Corredor <henry.corredor7570@gmail.com>
2025-01-21 17:48:14 -0500

    finalized visits model along with its routes and drivers

--- src/controllers/visitsController.js
+++ src/controllers/visitsController.js
@@ -34,12 +34,55 @@ export const getVisitById = async (req, res) => {
 export const createVisit = async (req,res) => {
     try {
         const { visit_date, userId, destinationId } = req.body;
-        if(!visit_date || !userId || !destinationId) return res.status(400).json({error:"Falta algún dato"})
-        const visit = await Visit.findOrCreate({
+        if(!visit_date || !userId || !destinationId) return res.status(400).json({error:"Falta algún dato"});
+
+        const user = await User.findByPk(userId);
+        const destination = await Destination.findByPk(destinationId);
+        if(!user) return res.status(404).json({error:"Usuario no encontrado."});
+        if(!destination) return res.status(404).json({error:"Destino no encontrado."});
+
+        const visit = await Visit.create({
+            visit_date,
+            user_id: userId,
+            destination_id: destinationId
+        });
+        res.status(200).json(visit);
+    } catch (error) {
+        res.status(500).json({error:error.message});
+    }
+}
+
+export const updateVisit = async (req,res) => {
+    try {
+        const { id } = req.params;
+        if(isNaN(id)) return res.status(400).json({error:"El ID debe ser un número."})
+        const { visit_date, userId, destinationId } = req.body;
+        if(!visit_date || !userId || !destinationId ) return res.status(500).json({error: "Falta por ingresar algún dato."})
+        const user = await User.findByPk(userId);
+        if(!user) return res.status(404).json({error:"Usuario no encontrado."})
+        const destination = await Destination.findByPk(destinationId);
+        if(!destination) return res.status(404).json({error:"Destino no encontrado."})
+        const visit = await Visit.findByPk(id);
+        if(!visit) return res.status(404).json({error: "Visita no encontrada."})
+        visit.update({
             visit_date,
             user_id: userId,
             destination_id: destinationId
         })
+        res.status(200).json(visit);
+    } catch (error) {
+        res.status(500).json({error:error.message});
+    }
+}
+
+export const deleteVisit = async (req,res) => {
+    try {
+        const {id} = req.params;
+        if(isNaN(id)) return res.status(400).json({error:"El ID debe ser un número."})
+        const visit = await Visit.findByPk(id);
+        if(!visit) return res.status(404).json({error:"visita no encontrada."});
+        await visit.destroy();
+        res.status(200).json({message:"Visita eliminada correctamente."});
     } catch (error) {
         res.status(500).json({error:error.message});
     }
--- src/routes/visitsRouter.js
+++ src/routes/visitsRouter.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 const visitsRouter = Router();
-import { createVisit, getAllVisits, getVisitById } from "../controllers/visitsController.js";
+import { createVisit, deleteVisit, getAllVisits, getVisitById, updateVisit } from "../controllers/visitsController.js";
 
 visitsRouter.get("/", getAllVisits);
 visitsRouter.get("/:id", getVisitById);
 visitsRouter.post("/", createVisit);
+visitsRouter.put("/:id", updateVisit);
+visitsRouter.delete("/:id", deleteVisit);
 
 export default visitsRouter;
