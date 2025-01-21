faf099b24d35b63a3642219e9922594649ca97a9
Henry Corredor <henry.corredor7570@gmail.com>
2025-01-21 16:12:13 -0500

    add visits routes

--- src/controllers/visitsController.js
+++ src/controllers/visitsController.js
@@ -13,3 +13,34 @@ export const getAllVisits = async (req, res) => {
         res.status(500).json({error:error.message});
     }
 }
+
+export const getVisitById = async (req, res) => {
+    const { id } = req.params;
+    try {
+        if(isNaN(id)) return res.status(400).json({error:"El ID proporcionado no es un número"});
+        const visit = await Visit.findByPk(id,{
+            include: [
+                {model:User, as:"user", attributes:["username"]},
+                {model:Destination, as:"destination", attributes: ["name"]}
+            ]
+        });
+        if(!visit) return res.status(404).json({error:"Visita no encontrada en la base de datos"});
+        res.status(200).json(visit);
+    } catch (error) {
+        res.status(500).json({error:error.message})
+    }
+}
+
+export const createVisit = async (req,res) => {
+    try {
+        const { visit_date, userId, destinationId } = req.body;
+        if(!visit_date || !userId || !destinationId) return res.status(400).json({error:"Falta algún dato"})
+        const visit = await Visit.findOrCreate({
+            visit_date,
+            user_id: userId,
+            destination_id: destinationId
+        })
+    } catch (error) {
+        res.status(500).json({error:error.message});
+    }
+}
--- src/routes/visitsRouter.js
+++ src/routes/visitsRouter.js
@@ -1,7 +1,9 @@
 import { Router } from "express";
 const visitsRouter = Router();
-import { getAllVisits } from "../controllers/visitsController.js";
+import { createVisit, getAllVisits, getVisitById } from "../controllers/visitsController.js";
 
 visitsRouter.get("/", getAllVisits);
+visitsRouter.get("/:id", getVisitById);
+visitsRouter.post("/", createVisit);
 
 export default visitsRouter;
