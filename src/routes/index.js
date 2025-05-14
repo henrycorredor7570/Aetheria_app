907386032b767c6f6c6c343a6331a11a54b29eb7
Henry Corredor <henry.corredor7570@gmail.com>
2025-05-14 13:27:08 -0500

    assigning middleware to protect routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,14 +5,13 @@ import reviewsRouter from "./reviewsRouter.js";
 import visitsRouter from "./visitsRouter.js";
 import pointsOfInterestRouter from "./pointsOfInterest.js";
 import ARRouter from "./ARRouter.js";
-import { authMiddleware } from "./../middlewares/authMiddleware.js";
 
 const router = Router();
 
-router.use("/users", authMiddleware, usersRouter);
+router.use("/users", usersRouter);
 router.use("/destinations", destinationsRouter);
 router.use("/reviews", reviewsRouter);
-router.use("/visits", authMiddleware, visitsRouter);
+router.use("/visits", visitsRouter);
 router.use("/pointsofinterest", pointsOfInterestRouter);
 router.use("/augmentedreality", ARRouter);
 
--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 const usersRouter = Router();
 import { getAllUsers, getUserById, createUser, updateUser, deleteUser, loginUser} from '../controllers/usersController.js'
+import { authMiddleware } from "./../middlewares/authMiddleware.js";
 
-usersRouter.get("", getAllUsers);
-usersRouter.get("/:id", getUserById);
+usersRouter.get("", authMiddleware, getAllUsers);
+usersRouter.get("/:id", authMiddleware, getUserById);
 usersRouter.post("", createUser);
-usersRouter.put("/:id", updateUser);
-usersRouter.delete("/:id", deleteUser);
+usersRouter.put("/:id", authMiddleware, updateUser);
+usersRouter.delete("/:id", authMiddleware, deleteUser);
 usersRouter.post("/login", loginUser);
 
 export default usersRouter;
