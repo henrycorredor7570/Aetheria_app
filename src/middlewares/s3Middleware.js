998a27c0967d93de05f5d0312c2381aa1a354e22
Henry Corredor <henry.corredor7570@gmail.com>
2026-03-27 11:59:12 -0500

    add comments

--- src/middlewares/s3Middleware.js
+++ src/middlewares/s3Middleware.js
@@ -2,6 +2,9 @@ import multer from "multer";
 import multerS3 from "multer-s3";
 import s3 from "../services/s3Service.js";
 
+/**
+ *  Multer-S3 intercepta archivos y los sube automáticamente
+ */
 const upload = multer({
     storage: multerS3({
         s3,
--- src/services/s3Service.js
+++ src/services/s3Service.js
@@ -1,5 +1,7 @@
 import AWS from "aws-sdk";
-
+/**
+ * Crea la conexión a AWS S3 con las credenciales
+ */
 const s3 = new AWS.S3({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
