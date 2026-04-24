7781a25044cff58129c832d8b49fbb463a4e1f96
Henry Corredor <henry.corredor7570@gmail.com>
2026-04-24 18:10:40 -0500

    add aws s3

--- src/controllers/destinationsController.js
+++ src/controllers/destinationsController.js
@@ -39,7 +39,10 @@ export const getDestinations = async (req, res) => {
 
 export const createDestination = async (req,res) => {
     const { name, description, latitude, longitude, country } = req.body;
+    console.log("BODY:", req.body);
+    console.log("FILE:", req.file);
     try {
+        console.log(name, description, latitude, longitude, country)
         if(!name || !description || !latitude || !longitude || !country){
             return res.status(400).json({error:"¡Hizo falta algún dato!"})
         }
--- src/services/s3Service.js
+++ src/services/s3Service.js
@@ -1,8 +1,8 @@
-import AWS from "aws-sdk";
+import { S3Client } from "@aws-sdk/client-s3";
 /**
  * Crea la conexión a AWS S3 con las credenciales
  */
-const s3 = new AWS.S3({
+const s3 = new S3Client({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
     region: process.env.AWS_REGION,
